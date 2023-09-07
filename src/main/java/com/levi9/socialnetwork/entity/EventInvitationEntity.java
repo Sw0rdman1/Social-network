7f9d50658a69cc7372fd5d35840a2dbc5d583834
Božidar <vujasinovicb2019@gmail.com>
2023-09-07 17:06:40 +0200

    Entity Event and all around it documented

---
+++ src/main/java/com/levi9/socialnetwork/entity/EventInvitationEntity.java
@@ -0,0 +1,44 @@
+/**
+ * Predstavlja entitet poziva na događaj u aplikaciji društvene mreže.
+ */
+package com.levi9.socialnetwork.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "event_invitation")
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class EventInvitationEntity {
+    /**
+     * Jedinstveni identifikator poziva na događaj.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * Član grupe koji je pozvan na događaj.
+     */
+    @ManyToOne
+    @JoinColumn(name = "invitee_id")
+    private GroupMemberEntity invitee;
+
+    /**
+     * Događaj za koji je poslat poziv.
+     */
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private EventEntity event;
+
+    /**
+     * Status poziva na događaj (NE DOLAZIM ili DOLAZIM).
+     */
+    @Column(columnDefinition = "ENUM('NOT_COMING', 'COMING')")
+    @Enumerated(EnumType.STRING)
+    private EventInvitationStatusEntity status;
+}
